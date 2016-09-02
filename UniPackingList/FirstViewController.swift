33852a92d2d1929d6433896c81e0fbec8a30a6bc
vrMars <vrmars98@gmail.com>
2016-09-02 11:06:31 -0400

    ToDO List App Swift 3

--- UniPackingList/FirstViewController.swift
+++ UniPackingList/FirstViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var todolist = [String]()
+
+
+class FirstViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if UserDefaults.standard.object(forKey: "list") != nil {
+            todolist = UserDefaults.standard.object(forKey:"list") as! [String]
+        }
+        self.tableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +30,41 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /*func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todolist.count
+    }*/
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        
+        cell.textLabel?.text = todolist [indexPath.row]
+        return cell
+    }
+    func tableView(_: UITableView, numberOfRowsInSection: Int) -> Int{
+        return todolist.count
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            todolist.remove(at:indexPath.row)
+            UserDefaults.standard.set(todolist, forKey: "list")
+            tableView.reloadData()
+            
+        }
+    
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+
+
 
 
 }
--- UniPackingList/SecondViewController.swift
+++ UniPackingList/SecondViewController.swift
@@ -8,16 +8,41 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+var Todolist = [String]()
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var textfield: UITextField!
+    @IBAction func additem (sender: AnyObject) {
+        todolist.append(textfield.text!)
+        textfield.text = ""
+        
+
+        
+        
+        UserDefaults.standard.set(todolist, forKey: "list")
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.textfield.delegate=self
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event : UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
